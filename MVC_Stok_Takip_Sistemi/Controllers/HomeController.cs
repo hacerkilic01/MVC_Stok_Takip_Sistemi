8367c3f347457fa0973c33c0ba2ce65245801d8b
Hacer Kılıç <hacerkilic194@gmail.com>
2025-01-30 14:46:18 +0300

    edit template

--- MVC_Stok_Takip_Sistemi/Controllers/HomeController.cs
+++ MVC_Stok_Takip_Sistemi/Controllers/HomeController.cs
@@ -15,14 +15,17 @@ namespace MVC_Stok_Takip_Sistemi.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Products()
         {
-            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Store()
+        {
 
             return View();
         }
