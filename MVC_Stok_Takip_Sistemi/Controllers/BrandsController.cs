0efb3a580dbb4f032599a69b267da79827ca1fe6
Hacer Kılıç <hacerkilic194@gmail.com>
2025-02-09 16:57:56 +0300

    update brand

--- MVC_Stok_Takip_Sistemi/Controllers/BrandsController.cs
+++ MVC_Stok_Takip_Sistemi/Controllers/BrandsController.cs
@@ -10,28 +10,35 @@ namespace MVC_Stok_Takip_Sistemi.Controllers
     public class BrandsController : Controller
     {
         // GET: Brands
-        MVC_Stok_TakibiEntities1 db = new MVC_Stok_TakibiEntities1 ();
+        MVC_Stok_TakibiEntities1 db = new MVC_Stok_TakibiEntities1();
         public ActionResult Index()
         {
-            var model =db.Brands.ToList();
+            var model = db.Brands.ToList();
             return View(model);
         }
         [HttpGet]
         public ActionResult Add()
         {
+            SelectUpdateInformation();
+            return View();
+        }
+
+        private void SelectUpdateInformation()
+        {
             var model = new Brand();
-            List<SelectListItem> list =new List<SelectListItem> (
-                from x in db.Categories select new SelectListItem
+            List<SelectListItem> list = new List<SelectListItem>(
+                from x in db.Categories
+                select new SelectListItem
                 {
                     Value = x.ID.ToString(),
                     Text = x.CategoryName
                 }
                 ).ToList();
-            ViewBag.l =list;
-            return View();
+            ViewBag.l = list;
         }
+
         [HttpPost]
-        public ActionResult Add (Brand m)
+        public ActionResult Add(Brand m)
         {
             if (!ModelState.IsValid)
             {
@@ -43,10 +50,35 @@ namespace MVC_Stok_Takip_Sistemi.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult UpdateInformation(int id)
+        {
+            SelectUpdateInformation();
+            var find = db.Brands.Find(id);
+            return View(find);
+        }
+        public ActionResult Update(Brand p)
+        {
+            if (!ModelState.IsValid)
+            {
+                SelectUpdateInformation();
+                return View("UpdateInformation");
+            }
+            db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public ActionResult DeleteBrand(Brand p)
         {
             var getir = db.Brands.Find(p.ID);
             return View(getir);
+        }
+
+        public ActionResult Delete(Brand p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
